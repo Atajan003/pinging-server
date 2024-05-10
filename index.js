e7e899bc7acafec12ac508e5e8d53f585e05c939
root <root@vm2038214.stark-industries.solutions>
2024-05-10 19:34:53 +0200

    Update repository

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
+const express = require('express')
+const app = express()
+
 console.log("pingin server is running to versel")
+
+
+app.use('/', express.static(path.join(__dirname, 'data'))) 
+
+app.listen(3000,(error)=>{
+    console.log(error ? error:"RUN SERVER localhost:3000")
+})
+
+
+
+// Export the Express API
+module.exports = app;
